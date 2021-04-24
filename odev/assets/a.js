76d55dcdc6d5c518725a3ab491036405f41b9f72
yusuf cengiz <yc977771@gmail.com>
2021-04-25 02:32:14 +0300

    başlık kısmının genel çerçevesi

---
+++ odev/assets/a.js
@@ -0,0 +1,12 @@
+var min = document.getElementById("alt").value;
+var max = document.getElementById("ust").value;
+
+
+
+function sayi(min, max) {
+    var x = document.getElementById("sonuc");
+
+
+
+
+}
