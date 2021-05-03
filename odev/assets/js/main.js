fb1769bb2d0050673960aa7376dfd254a3708c2c
yusuf cengiz <yc977771@gmail.com>
2021-05-03 11:58:21 +0300

    yeni navbar

--- odev/assets/js/main.js
+++ odev/assets/js/main.js
@@ -1,12 +1,85 @@
-var min = document.getElementById("alt").value;
-var max = document.getElementById("ust").value;
+//  function ac() {
+
+//      document.getElementById("e").style.height = "100px";
+//  }
+
+//  function kapat() {
+//      document.getElementById("e").style.height = "0px";
+//  }
+
+$(document).ready(function() {
+    $("#ev").mouseover(function() {
+        $("#evBahce").css("display", "block");
+    });
+    $("#ev").mouseout(function() {
+        $("#evBahce").css("display", "none");
+    });
+
+
+    $("#elektronik").mouseover(function() {
+        $("#moda").css("display", "block");
+    });
+    $("#elektronik").mouseout(function() {
+        $("#moda").css("display", "none");
+    });
 
 
 
-function sayi(min, max) {
-    var x = document.getElementById("sonuc");
+    $("#elektronik").mouseover(function() {
+        $("#kozmetik").css("display", "block");
+    });
+    $("#elektronik").mouseout(function() {
+        $("#kozmetik").css("display", "none");
+    });
+
+
+    $("#elektronik").mouseover(function() {
+        $("#anneBebek").css("display", "block");
+    });
+    $("#elektronik").mouseout(function() {
+        $("#anneBebek").css("display", "none");
+    });
+
+
+    $("#elektronik").mouseover(function() {
+        $("#supermarket").css("display", "block");
+    });
+    $("#elektronik").mouseout(function() {
+        $("#supermarket").css("display", "none");
+    });
 
 
 
 
-}
+    $("#ev").mouseover(function() {
+        $("#evBahce").css("display", "block");
+    });
+    $("#ev").mouseout(function() {
+        $("#evBahce").css("display", "none");
+    });
+
+
+
+
+});
+$(document).ready(function() {
+
+    $("#elektronik").mouseover(function() {
+        $("#e").css("display", "block");
+    });
+    $("#elektronik").mouseout(function() {
+        $("#e").css("display", "none");
+
+
+    });
+});
+
+
+$(document).ready(function() {
+    $("#modaac").mouseover(function() {
+        $("#moda").css("display", "block");
+    });
+    $("#modaac").mouseout(function() {
+        $("#moda").css("display", "none");
+    });
+});
