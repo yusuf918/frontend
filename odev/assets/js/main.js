7aef18b98b650e2962edbdf4cf5bea883114f2cb
yusuf cengiz <yc977771@gmail.com>
2021-05-04 18:22:13 +0300

    main kısmı çerçeve

--- odev/assets/js/main.js
+++ odev/assets/js/main.js
@@ -1,94 +1,104 @@
-// //  function ac() {
-
-// //      document.getElementById("e").style.height = "100px";
-// //  }
-
-// //  function kapat() {
-// //      document.getElementById("e").style.height = "0px";
-// //  }
-
-// $(document).ready(function() {
-//     $("#ev").mouseover(function() {
-//         $("#evBahce").css("display", "block");
-//     });
-//     $("#ev").mouseout(function() {
-//         $("#evBahce").css("display", "none");
-//     });
-
-
-//     $("#elektronik").mouseover(function() {
-//         $("#moda").css("display", "block");
-//     });
-//     $("#elektronik").mouseout(function() {
-//         $("#moda").css("display", "none");
-//     });
-
-
-
-//     $("#elektronik").mouseover(function() {
-//         $("#kozmetik").css("display", "block");
-//     });
-//     $("#elektronik").mouseout(function() {
-//         $("#kozmetik").css("display", "none");
-//     });
-
-
-//     $("#elektronik").mouseover(function() {
-//         $("#anneBebek").css("display", "block");
-//     });
-//     $("#elektronik").mouseout(function() {
-//         $("#anneBebek").css("display", "none");
-//     });
-
-
-//     $("#elektronik").mouseover(function() {
-//         $("#supermarket").css("display", "block");
-//     });
-//     $("#elektronik").mouseout(function() {
-//         $("#supermarket").css("display", "none");
-//     });
-
-
-
-
-//     $("#ev").mouseover(function() {
-//         $("#evBahce").css("display", "block");
-//     });
-//     $("#ev").mouseout(function() {
-//         $("#evBahce").css("display", "none");
-//     });
-
-
-
-
-// });
-// $(document).ready(function() {
-
-//     $("#elektronik").mouseover(function() {
-//         $("#e").css("display", "block");
-//     });
-//     $("#elektronik").mouseout(function() {
-//         $("#e").css("display", "none");
-
-
-//     });
-// });
-
-
-// $(document).ready(function() {
-//     $("#modaac").mouseover(function() {
-//         $("#moda").css("display", "block");
-//     });
-//     $("#modaac").mouseout(function() {
-//         $("#moda").css("display", "none");
-//     });
-// });
-
+// varsayılan main kısmı
 $(document).ready(function() {
-    $("ul").mouseover(function() {
-        $("#asd").css("display", "none");
+    $("li").mouseover(function() {
+        $("#varsayilan").css("display", "none");
     });
-    $("ul").mouseout(function() {
-        $("#asd").css("display", "block");
+    $("li").mouseout(function() {
+        $("#varsayilan").css("display", "block");
+    });
+});
+
+// elektronik kısmı
+$(document).ready(function() {
+    $("#liElektronik").mouseover(function() {
+        $("#elektronik").css("display", "block");
+    });
+    $("#liElektronik").mouseout(function() {
+        $("#elektronik").css("display", "none");
+    });
+});
+
+// evbahce kısmı
+$(document).ready(function() {
+
+    $("#liEvBahce").mouseover(function() {
+        $("#evBahce").css("display", "block");
+    });
+    $("#liEvBahce").mouseout(function() {
+        $("#evBahce").css("display", "none");
+
+
+    });
+});
+
+// moda kısmı
+$(document).ready(function() {
+
+    $("#liModa").mouseover(function() {
+        $("#moda").css("display", "block");
+    });
+    $("#liModa").mouseout(function() {
+        $("#moda").css("display", "none");
+    });
+});
+
+//kozmetik kısmı
+$(document).ready(function() {
+    $("#liKozmetik").mouseover(function() {
+        $("#kozmetik").css("display", "block");
+    });
+    $("#liKozmetik").mouseout(function() {
+        $("#kozmetik").css("display", "none");
+    });
+});
+
+//anne bebek
+$(document).ready(function() {
+    $("#liAnneBebek").mouseover(function() {
+        $("#anneBebek").css("display", "block");
+    });
+    $("#liAnneBebek").mouseout(function() {
+        $("#anneBebek").css("display", "none");
+    });
+});
+
+//supermarket
+$(document).ready(function() {
+    $("#liSuperMarket").mouseover(function() {
+        $("#superMarket").css("display", "block");
+    });
+    $("#liSuperMarket").mouseout(function() {
+        $("#superMarket").css("display", "none");
+    });
+});
+
+// kitap muzik
+$(document).ready(function() {
+    $("#liKitapMuzik").mouseover(function() {
+        $("#kitapMuzik").css("display", "block");
+    });
+    $("#liKitapMuzik").mouseout(function() {
+        $("#kitapMuzik").css("display", "none");
+    });
+});
+
+// otomobil bisiklet
+$(document).ready(function() {
+    $("#liOtomobilBisiklet").mouseover(function() {
+        $("#otomobilBisiklet").css("display", "block");
+    });
+    $("#liOtomobilBisiklet").mouseout(function() {
+        $("#otomobilBisiklet").css("display", "none");
+    });
+
+});
+
+// spor outdoor
+$(document).ready(function() {
+    $("#liSporOutdoor").mouseover(function() {
+        $("#sporOutdoor").css("display", "block");
+    });
+    $("#liSporOutdoor").mouseout(function() {
+        $("#sporOutdoor").css("display", "none");
     });
 });
