8204a3dd131877e7009bededab38cafaad633360
yusuf cengiz <yc977771@gmail.com>
2021-05-04 14:31:23 +0300

    main baslangıc

--- odev/assets/js/main.js
+++ odev/assets/js/main.js
@@ -1,85 +1,94 @@
-//  function ac() {
+// //  function ac() {
 
-//      document.getElementById("e").style.height = "100px";
-//  }
+// //      document.getElementById("e").style.height = "100px";
+// //  }
 
-//  function kapat() {
-//      document.getElementById("e").style.height = "0px";
-//  }
+// //  function kapat() {
+// //      document.getElementById("e").style.height = "0px";
+// //  }
+
+// $(document).ready(function() {
+//     $("#ev").mouseover(function() {
+//         $("#evBahce").css("display", "block");
+//     });
+//     $("#ev").mouseout(function() {
+//         $("#evBahce").css("display", "none");
+//     });
+
+
+//     $("#elektronik").mouseover(function() {
+//         $("#moda").css("display", "block");
+//     });
+//     $("#elektronik").mouseout(function() {
+//         $("#moda").css("display", "none");
+//     });
+
+
+
+//     $("#elektronik").mouseover(function() {
+//         $("#kozmetik").css("display", "block");
+//     });
+//     $("#elektronik").mouseout(function() {
+//         $("#kozmetik").css("display", "none");
+//     });
+
+
+//     $("#elektronik").mouseover(function() {
+//         $("#anneBebek").css("display", "block");
+//     });
+//     $("#elektronik").mouseout(function() {
+//         $("#anneBebek").css("display", "none");
+//     });
+
+
+//     $("#elektronik").mouseover(function() {
+//         $("#supermarket").css("display", "block");
+//     });
+//     $("#elektronik").mouseout(function() {
+//         $("#supermarket").css("display", "none");
+//     });
+
+
+
+
+//     $("#ev").mouseover(function() {
+//         $("#evBahce").css("display", "block");
+//     });
+//     $("#ev").mouseout(function() {
+//         $("#evBahce").css("display", "none");
+//     });
+
+
+
+
+// });
+// $(document).ready(function() {
+
+//     $("#elektronik").mouseover(function() {
+//         $("#e").css("display", "block");
+//     });
+//     $("#elektronik").mouseout(function() {
+//         $("#e").css("display", "none");
+
+
+//     });
+// });
+
+
+// $(document).ready(function() {
+//     $("#modaac").mouseover(function() {
+//         $("#moda").css("display", "block");
+//     });
+//     $("#modaac").mouseout(function() {
+//         $("#moda").css("display", "none");
+//     });
+// });
 
 $(document).ready(function() {
-    $("#ev").mouseover(function() {
-        $("#evBahce").css("display", "block");
+    $("ul").mouseover(function() {
+        $("#asd").css("display", "none");
     });
-    $("#ev").mouseout(function() {
-        $("#evBahce").css("display", "none");
-    });
-
-
-    $("#elektronik").mouseover(function() {
-        $("#moda").css("display", "block");
-    });
-    $("#elektronik").mouseout(function() {
-        $("#moda").css("display", "none");
-    });
-
-
-
-    $("#elektronik").mouseover(function() {
-        $("#kozmetik").css("display", "block");
-    });
-    $("#elektronik").mouseout(function() {
-        $("#kozmetik").css("display", "none");
-    });
-
-
-    $("#elektronik").mouseover(function() {
-        $("#anneBebek").css("display", "block");
-    });
-    $("#elektronik").mouseout(function() {
-        $("#anneBebek").css("display", "none");
-    });
-
-
-    $("#elektronik").mouseover(function() {
-        $("#supermarket").css("display", "block");
-    });
-    $("#elektronik").mouseout(function() {
-        $("#supermarket").css("display", "none");
-    });
-
-
-
-
-    $("#ev").mouseover(function() {
-        $("#evBahce").css("display", "block");
-    });
-    $("#ev").mouseout(function() {
-        $("#evBahce").css("display", "none");
-    });
-
-
-
-
-});
-$(document).ready(function() {
-
-    $("#elektronik").mouseover(function() {
-        $("#e").css("display", "block");
-    });
-    $("#elektronik").mouseout(function() {
-        $("#e").css("display", "none");
-
-
-    });
-});
-
-
-$(document).ready(function() {
-    $("#modaac").mouseover(function() {
-        $("#moda").css("display", "block");
-    });
-    $("#modaac").mouseout(function() {
-        $("#moda").css("display", "none");
+    $("ul").mouseout(function() {
+        $("#asd").css("display", "block");
     });
 });
