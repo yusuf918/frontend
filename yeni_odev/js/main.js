d28c008d6be4a2754e14ac2a5cbb0a9eb0e7b95a
yusuf cengiz <yc977771@gmail.com>
2021-05-11 22:41:27 +0300

    nav kısmı biraz  guncelendi

--- yeni_odev/js/main.js
+++ yeni_odev/js/main.js
@@ -1,3 +1,8 @@
+// header turuncu dairedeki sayi icin
 var headerSayi = document.getElementById("headerSayi");
 var sayi = "0";
 headerSayi.innerHTML = sayi;
+
+function headerAsagiKaydir() {
+    document.write("rdctfvgyhbj");
+}
