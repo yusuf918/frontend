55bd8a739b6e09c848e8580d50b304c173ad9daa
yusuf cengiz <yc977771@gmail.com>
2021-06-22 18:48:14 +0300

    footer fonksiiyonu yapmaya başladım

--- yeniOdev/assets/js/main.js
+++ yeniOdev/assets/js/main.js
@@ -110,13 +110,150 @@ $(".emigreMagazineLg").html($(".emigreMagazineAcilim").html());
 $(".merchMoreLg").html($(".merchMoreAcilim").html());
 
 // footer kısmındaki kısmımlar için
-$(".licensing").click(function() {
-    $(".licensingAcilim").toggle();
+
+
+$('html').click(function() {
+    $(".typeDesignersAcilim").hide();
 });
-$(".typeDesigners").click(function() {
+$(".typeDesigners").click(function(e) {
+    e.stopPropagation();
     $(".typeDesignersAcilim").toggle();
+
 });
 
-// $(div[class != "licensing"]).click(function() {
-//     $(".licensingAcilim").hide();
-// });
+
+
+$('html').click(function() {
+    $(".licensingAcilim").hide();
+});
+$(".licensing").click(function(e) {
+    e.stopPropagation();
+    $(".licensingAcilim").toggle();
+
+});
+
+function footer() {
+    function col12() {
+        function aboutEmigre() {
+            let aboutEmigre = '<div class="header"><div class="fw-bold pb-0 pt-2 mb-0">About Emigre</div></div>';
+            return aboutEmigre;
+        }
+
+        function content() {
+            let yazi = "Emigre, Inc. is a digital type foundry based in Berkeley, California. Founded in 1984, coinciding with the birth of the Macintosh computer, the Emigre team, consisting of Rudy VanderLans and Zuzana Licko, with the addition of Tim Starback in 1993, were among the early adaptors to the new technology. ";
+            let aboutHtmlLinki = '<span> <a href="About.html" class="text-decoration-none text-white">(...more)</a></span>';
+
+            let content = '<div class="content small">' + yazi + aboutHtmlLinki + '</div>';
+
+            return content;
+
+        }
+
+        function footer() {
+            return '<div class="footer pt-4"><img src="./assets/png/EmigreFontsLogo.png" width="100" alt=""></div>';
+        }
+
+        var asd = aboutEmigre() + content() + footer();
+        return '<div class="col-12 col-md-6  px-0  ">' + asd + '</div>';
+    }
+
+    function col6() {
+        function generalInfo() {
+            let h6 = '  <h6 class="fw-bold">General Info</h6>';
+            return h6;
+        }
+
+        function div() {
+            function licensing() {
+                let div = '<div class="licensing" style="height: 20px;">';
+
+                function licensingAcilim() {
+
+
+                    function overwiew() {
+                        let overwiew = '<div class="border-bottom border-secondary ps-4 text-dark">overwiew</div>';
+                        let link = '<a href="./license/license.html" class="text-decoration-none">';
+                        return link + overwiew + '</a>';
+                    }
+
+                    function desktop() {
+                        let desktop = '<div class="border-bottom border-secondary ps-4 text-dark">desktop</div>';
+                        let link = '<a href="./license/desktop.html" class="text-decoration-none">';
+                        return link + desktop + '</a>';
+
+                    }
+
+                    function web() {
+                        let web = '<div class="border-bottom border-secondary ps-4 text-dark">web</div>';
+                        let link = '<a href="./license/web.html" class="text-decoration-none">';
+                        return link + web + '</a>';
+                    }
+
+                    function app() {
+                        let app = '<div class="border-bottom border-secondary ps-4 text-dark">app</div>';
+                        let link = '<a href="./license/app.html" class="text-decoration-none">';
+                        return link + app + '</a>';
+                    }
+
+                    function ebook() {
+                        let ebook = '<div class="border-bottom border-secondary ps-4 text-dark">ebook</div>';
+                        let link = '<a href="./license/ebook.html" class="text-decoration-none">';
+                        return link + ebook + '</a>';
+                    }
+
+                    function server() {
+                        let server = '<div class="border-bottom border-secondary ps-4 text-dark">server</div>';
+                        let link = '<a href="./license/server.html" class="text-decoration-none">';
+                        return link + server + '</a>';
+                    }
+
+                    function game() {
+                        let game = '<div class="border-bottom border-secondary ps-4 text-dark">game</div>';
+                        let link = '<a href="./license/game.html" class="text-decoration-none">';
+                        return link + game + '</a>';
+                    }
+
+                    function worldWide() {
+                        let worldWide = ' <div class="border-bottom border-secondary ps-4 text-dark">World-Wide</div>';
+                        let link = '<a href="./license/world-wide.html" class="text-decoration-none">';
+                        return link + worldWide + '</a>';
+
+                    }
+
+                    function custom() {
+                        let custom = '<div class="border-bottom border-secondary ps-4 text-dark">custom</div>';
+                        let link = '<a href="./license/custom.html" class="text-decoration-none">';
+                        return link + custom + '</a>';
+                    }
+                    let div = '<div class="licensingAcilim position-relative bg-white text-dark ">';
+                    return div + generalInfo() + overwiew() + desktop() + web() + app() + ebook() + server() + game() + worldWide() + custom() + '</div>';
+
+
+
+                }
+                return div + licensingAcilim() + '</div>'
+
+            }
+            return '</div>' + licensing() + '</Div>';
+        }
+        return div();
+
+        function typeDesigners() {
+
+
+
+
+
+
+
+        }
+
+
+    }
+    document.getElementById("footer").innerHTML = col6();
+
+}
+
+
+
+footer();
