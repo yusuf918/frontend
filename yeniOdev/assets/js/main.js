16f603b29fac470000a20fbe223caa922838b629
yusuf cengiz <yc977771@gmail.com>
2021-07-29 20:29:07 +0300

    ana sayfa bitti

--- yeniOdev/assets/js/main.js
+++ yeniOdev/assets/js/main.js
@@ -33,10 +33,21 @@ $(".fw-bold").hover(function() {
         $(".fw-bold").css("font-size")
     })
 })
-const cursor = $(".cursor");
-
+const cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", (e) => {
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
+})
+$(".fa-expand,.fa-compress").click(function() {
+    $(".fa-expand,.fa-compress").toggleClass("d-none")
+})
+$(".fa-expand").click(function() {
+    var viewer = $(".fa-expand")[0];
+    var rFS = viewer.mozRequestFullScreen || viewer.webkitRequestFullscreen || viewer.requestFullscreen;
+    rFS.call(viewer);
+})
+$(".fa-compress").click(function() {
+    document.exitFullscreen()
 
+    return false;
 })
