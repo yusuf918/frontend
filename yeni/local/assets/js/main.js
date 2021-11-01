477e6d378ef53bb99911b81dfee8b3ea14a24070
yusuf cengiz <yc977771@gmail.com>
2021-11-01 22:24:33 +0300

    header 4e kadar geldim

--- yeni/local/assets/js/main.js
+++ yeni/local/assets/js/main.js
@@ -1,29 +1,3 @@
-function header6() {
-    const header6Button = [
-        ["GET STARTED", " bg-warning"],
-        ["MORE INFO", " bg-white"],
-        ["CONTACT US", " bg-purple"]
-    ];
-    const colTemplate = document.getElementById("header6Col")
-    var content;
-    for (let i = 0; i < header6Button.length; i++) {
-
-        let col = document.importNode(colTemplate.content, true);
-
-        //   yazı ve class ayarlama
-        col.querySelector("button").textContent = header6Button[i][0]
-        col.querySelector("button").className += header6Button[i][1]
-            //   bitti
-
-        $(".header6").append(col)
-
-    }
-    let Class = "row justify-content-center";
-    let div = $("<div>").addClass(Class);
-
-    $(".header6 .col-12").wrapAll(div)
-
-
-
-}
-header6();
+console.log("a")
+$("img").addClass("mb-4")
+console.log("a")
