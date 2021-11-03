9b8f11ac64105531f107b1f92b68c97be712b247
yusuf cengiz <yc977771@gmail.com>
2021-11-03 20:24:42 +0300

    sayfa içi yonlendirme alert iphone svg kaldı

--- yeni/global/assets/js/main.js
+++ yeni/global/assets/js/main.js
@@ -4,3 +4,10 @@ const source = "./assets/img/";
 $("img").each(function() {
     $(this).attr("src", source + $(this).attr("src"))
 })
+$(".appBadge").clone().appendTo(".header-6 .mt-4")
+$(".nav-item img").removeClass("mb-4").addClass("me-2")
+$(".topButton").click(function() {
+    $("html").animate({}, 100)
+
+
+})
