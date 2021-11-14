f766d39a0d58e670d130f01e4b5cfafd6c944b56
yusuf cengiz <yc977791@gmail.com>
2021-11-14 21:27:33 +0300

    about bitti

--- yeni/global/assets/js/main.js
+++ yeni/global/assets/js/main.js
@@ -1,13 +1,26 @@
-$("img").addClass("mb-4")
+// times
+function timesAlignAppend() {
 
-const source = "./assets/img/";
-$("img").each(function() {
-    $(this).attr("src", source + $(this).attr("src"))
-})
-$(".appBadge").clone().appendTo(".header-6 .mt-4")
-$(".nav-item img").removeClass("mb-4").addClass("me-2")
-$(".topButton").click(function() {
-    $("html").animate({}, 100)
+    $(".w-30").append($("<div>").addClass("bg-seaGreen h-3"))
+}
+timesAlignAppend();
+timesAlignAppend();
+timesAlignAppend();
+$(".nav-link,.navbar-brand").addClass("py-3")
+$(window).scroll(function() {
+        let height = $(this).scrollTop();
 
+        if (height > 100) {
+            $(".navbar").css("background-color", "white")
+            $(".nav-item .nav-link").css("color", "black")
+            $(".navbar .navbar-brand").css("color", "black")
+        } else {
+            $(".navbar").css({ "background-color": "transparent", })
+            $(".nav-item .nav-link").css("color", "rgba(255,255,255,0.5)")
+            $(".navbar .navbar-brand").css("color", "rgba(255,255,255,0.5)")
 
-})
+        }
+    })
+    // times bitti
+    // $("header p,header button").wrap()
+    //.container center
